e1bb957ee797747f2a84f3a49ee4bebd5e0d6c91
abdobarakat1 <ab7028084@gmail.com>
2021-10-17 20:44:00 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:untitled7/Categories.dart';
+import 'package:untitled7/Form.dart';
+import 'package:untitled7/Home.dart';
+import 'package:untitled7/Homes.dart';
+void main(){
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Forma(),
+
+
+      )
+  );
+
+
+}
